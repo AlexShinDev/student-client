d1edb19827b0a66707278a770b48fef7c2e6d463
ProfBologna <sam.aporia@gmail.com>
2017-10-31 18:50:34 -0500

    Trying to fix

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,26 +1,20 @@
 class Student
-  attr_accessor :first_name
+  attr_accessor :first_name, :last_name, :capstones, :description
 
   def initialize(options_hash)
-    @first_name = options_hash[:first_name]
+    @first_name = options_hash["first_name"]
+    @last_name = options_hash["last_name"]
+    @capstones = options_hash["capstones"]
+
   end
 
   def self.all 
     students = []
-    response = [
-                {
-                  "first_name": "Sam"
-                },
-                {
-                  "first_name": "Neil"
-                },
-                {
-                  "first_name": "Alex"
-                }
-              ]
+    response = Unirest.get(
+                          "https://crypto-currents-squidshack.herokuapp.com/api/v1/students.json",
+                          ).body
     response.each do |student_hash|
-      p student_hash
-      students << Student.new(student_hash)
+      students << student = Student.new(student_hash)
     end
     students
   end
